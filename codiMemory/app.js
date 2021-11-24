2fd287405cf06e1719e39b2e2f77efda105fd902
DamataVeloper <damatadeveloper@gmail.com>
2021-11-24 19:13:36 -0300

    imagens do jogo e contruindo array de objetos das imagens

--- codiMemory/app.js
+++ codiMemory/app.js
@@ -1,3 +1,53 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    
+    // carregamento dos cards
+    const cardArray = [
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+        {
+            name: 'direita',
+            img: 'images/direita.png'
+        },
+        {
+            name: 'direita',
+            img: 'images/direita.png'
+        },
+        {
+            name: 'tras',
+            img: 'images/tras.png'
+        },
+        {
+            name: 'tras',
+            img: 'images/tras.png'
+        },
+        {
+            name: 'correndo',
+            img: 'images/correndo.png'
+        },
+        {
+            name: 'correndo',
+            img: 'images/correndo.png'
+        },
+        {
+            name: 'pulo',
+            img: 'images/pulo.png'
+        },
+        {
+            name: 'pulo',
+            img: 'images/pulo.png'
+        },
+        {
+            name: 'esquerda',
+            img: 'images/esquerda.png'
+        },
+        {
+            name: 'esquerda',
+            img: 'images/esquerda.png'
+        }
+    ]
 })
